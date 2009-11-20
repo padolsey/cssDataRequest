f8b28ffccf3146f1a84f5a6f77c1fc99dadb1bf2
James Padolsey <jamespadolsey@gmail.com>
2009-11-20 01:45:34 +0000

    Updated, supported in pretty-much all browsers now

--- cssdatarequest.js
+++ cssdatarequest.js
@@ -2,7 +2,7 @@
  * @author James Padolsey
  * @copyright Copyright (c) 2009 James Padolsey
  * @license http://www.apache.org/licenses/LICENSE-2.0.html
- * @version 0.1
+ * @version 0.2
  */
 
 var cssDataRequest = (function(){
@@ -21,18 +21,33 @@ var cssDataRequest = (function(){
         
         try {
             
-            /* WebKit */
-            
             var styleSheets = document.styleSheets || [],
                 i = 0, ss;
-                
+            
             while ( (ss = styleSheets[i++]) ) {
-                if ( ss.href === url ) {
+                
+                if ( ss.href !== url ) { continue; }
+                
+                if ( ss.cssRules && ss.cssRules[0] ) {
+                    
+                    /* WebKit */
                     return ss.cssRules[0].style.content;
+                    
+                } else {
+                    
+                    /* IE */
+                    return (ss.cssText.match(/CONTENT\s*:\s*('|")((?:\\\1|[^\1])+)\1/i)||[,,''])[2];
+                    
                 }
             }
             
-        } catch(e) { /* Throws error in IE */ }
+        } catch(e) {
+            
+            /* Throws error in FF<3 = SECURITY ERROR */
+            var ff = getStyle(dataEl, 'fontFamily');
+            return cssDataRequest.FF2 ? ff.indexOf('data') === 1 ? ff.replace(/data/,'') : null : '';
+            
+        }
         
         return '';
         
@@ -41,11 +56,11 @@ var cssDataRequest = (function(){
     var head = document.getElementsByTagName('head')[0],
         body = document.body;
     
-    return function(url, callback) {
+    return function cssDataRequest(url, callback) {
         
         var dataEl = document.createElement('data'),
             link = document.createElement('link'),
-            timer;
+            timer, attempts = 0;
             
         link.rel = 'stylesheet';
         link.type = 'text/css';
@@ -58,6 +73,11 @@ var cssDataRequest = (function(){
         timer = setInterval(function(){
             
             var content = getContent(dataEl, url);
+            
+            if (attempts++ > 100) {
+                clearInterval(timer);
+                return;
+            }
             
             if (content && content !== 'none') {
                 
@@ -79,6 +99,7 @@ var cssDataRequest = (function(){
                 
                 callback(content);
             }
+            
         }, 100);
         
         return true;
